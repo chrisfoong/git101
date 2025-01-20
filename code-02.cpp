083d24acf75fff3115a1672b91b94091db2a2702
chrisfoong <chris.owo@hotmail.com>
2025-01-20 14:27:15 +0700

    update in class

--- code-02.cpp
+++ code-02.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main() {
-    string text = "Hello Git in C++";
-    cout << text << endl;
+    cout << "Hello C++" << endl;
     return 0;
 }
